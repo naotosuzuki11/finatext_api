b16e6d2c17418f6b02f1ea9eecfc38fd6c2ad5b3
Naoto Suzuki <naoto.n.suzuki@gmail.com>
2020-03-08 22:18:51 -0600

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'csv'
-# User.destroy_all
-# Comment.destroy_all
+User.destroy_all
+Comment.destroy_all
+CommentScore.destroy_all
 
 
 
-# (0..10).each do |number|
-#   uri = "https://interview-external.moneywelfare.com/users/#{number}"
-#   user_data = HTTParty.get(uri)
-#   user = User.new(id: user_data["id"], name: user_data["name"])
-#   if user.valid?
-#     user.save!
-#   else
-#   end
-# end
+(0..10).each do |number|
+  uri = "https://interview-external.moneywelfare.com/users/#{number}"
+  user_data = HTTParty.get(uri)
+  user = User.new(id: user_data["id"], name: user_data["name"])
+  if user.valid?
+    user.save!
+  else
+  end
+end
 
 CSV.foreach('db/commentscores.csv') do |row|
   comment_score = CommentScore.create!(comment: Comment.find(row[0]), score_type: row[1], score: row[2])
@@ -31,10 +32,10 @@ CSV.foreach('db/commentscores.csv') do |row|
 end
 
 
-# CSV.foreach('db/usercomments.csv') do |row|
-#   comment = Comment.new(id: row[0], user: User.find(row[1]), comment_date: row[2], text: row[3])
-#   if comment.valid?
-#     comment.save
-#   else
-#   end
-# end
+CSV.foreach('db/usercomments.csv') do |row|
+  comment = Comment.new(id: row[0], user: User.find(row[1]), comment_date: row[2], text: row[3])
+  if comment.valid?
+    comment.save
+  else
+  end
+end
